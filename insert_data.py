906deffa500dc31ce84446154ae545298cf95229
KelevraP5 <kelevrap5@gmail.com>
2026-08-18 11:19:09 +0200

    modif bdd, drop duplicates pieces, insertion google sheets

--- insert_data.py
+++ insert_data.py
@@ -2,7 +2,13 @@ from database import (
     insert_clean_data,
     insert_sql_to_sheets,
 )
-from queries import antenne_query, employes_query, garage_query, loc_pret_query, pieces_query
+from queries import (
+    antenne_query,
+    employes_query,
+    garage_query,
+    loc_pret_query,
+    pieces_query,
+)
 from rename_columns import (
     data_client,
     data_employes,
@@ -19,23 +25,23 @@ spreadsheet_name = "Exercice Data Analyst"
 
 # Insertion en BDD Clean
 
-# insert_clean_data(data_client, "client")
-# insert_clean_data(data_pieces, "pieces")
-# insert_clean_data(data_employes, "employes")
-# insert_clean_data(data_voiture_client, "voiture_client")
-# insert_clean_data(data_voiture, "voitures")
-# insert_clean_data(data_loc_pret, "location_pret")
-# insert_clean_data(data_rep, "reparations")
-# insert_clean_data(data_rep_pieces, "reparation_pieces")
+insert_clean_data(data_client, "client")
+insert_clean_data(data_pieces, "pieces")
+insert_clean_data(data_employes, "employes")
+insert_clean_data(data_voiture_client, "voiture_client")
+insert_clean_data(data_voiture, "voitures")
+insert_clean_data(data_loc_pret, "location_pret")
+insert_clean_data(data_rep, "reparations")
+insert_clean_data(data_rep_pieces, "reparation_pieces")
 
 # Insertion dans un Google Sheets
 
-# insert_sql_to_sheets(credentials_file_name=credentials_file, spreadsheet_name=spreadsheet_name, sheet_name="DATA-employés", custom_query=employes_query, begin_pos= "A1")
+insert_sql_to_sheets(credentials_file_name=credentials_file, spreadsheet_name=spreadsheet_name, sheet_name="DATA-employés", custom_query=employes_query, begin_pos= "A1")
 
-# insert_sql_to_sheets(credentials_file_name=credentials_file, spreadsheet_name=spreadsheet_name, sheet_name="DATA-garages & antennes", custom_query=garage_query, begin_pos= "A1")
+insert_sql_to_sheets(credentials_file_name=credentials_file, spreadsheet_name=spreadsheet_name, sheet_name="DATA-garages & antennes", custom_query=garage_query, begin_pos= "A1")
 
-# insert_sql_to_sheets(credentials_file_name=credentials_file, spreadsheet_name=spreadsheet_name, sheet_name="DATA-garages & antennes", custom_query=antenne_query, begin_pos="A9")
+insert_sql_to_sheets(credentials_file_name=credentials_file, spreadsheet_name=spreadsheet_name, sheet_name="DATA-garages & antennes", custom_query=antenne_query, begin_pos="A9")
 
-# insert_sql_to_sheets(credentials_file_name=credentials_file, spreadsheet_name=spreadsheet_name, sheet_name="DATA-locations & prêts", custom_query=loc_pret_query, begin_pos="A1")
+insert_sql_to_sheets(credentials_file_name=credentials_file, spreadsheet_name=spreadsheet_name, sheet_name="DATA-locations & prêts", custom_query=loc_pret_query, begin_pos="A1")
 
-# insert_sql_to_sheets(credentials_file_name=credentials_file, spreadsheet_name=spreadsheet_name, sheet_name="DATA-pièces", custom_query=pieces_query, begin_pos="A1")
+insert_sql_to_sheets(credentials_file_name=credentials_file, spreadsheet_name=spreadsheet_name, sheet_name="DATA-pièces", custom_query=pieces_query, begin_pos="A1")
